3cd6017e0ba72ce6e18741ef0b0365f3c1331d86
kobia <kobiakithinji16@gmail.com>
2022-09-15 15:08:57 -0700

    changes

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,26 +1,41 @@
 #include "main.h"
 
 /**
- * main-start point
  *
- * Description: function check lower charater
+ *  * main-start point
  *
- * Return: retun 1 if in lower or 0 otherwise
- */
+ *   * Description: function check lower charater
+ *
+ *    * Return: retun 1 if in lower or 0 otherwise
+ *
+ *     */
 
 int _islower(int c)
+
 {
-int n ='q';
-if ( islower (n);
-{
-printf ("%c is a lowercase\n",n);
-}
-else 
-{
-printf ("%cin not in lowercase\n",n);
-}
+
+		int n ='q';
+
+			if ( islower (n))
+
+					{
+
+								printf ("%c is a lowercase\n",n);
+
+									}
+
+				else 
+
+						{ 
+
+									printf ("%cin not in lowercase\n",n);
+
+										}
+
+					
+
 }
 
 
 
-i
+
