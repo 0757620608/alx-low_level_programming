04da09ed18635b164343ced479c06643ee500980
eutychus kuria <eutychuskuria@gimail.com>
2022-09-21 18:18:51 +0300

    corrections

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,23 +1,19 @@
 #include "main.h"
 /**
- *  * Description: function prints alphabetic characters
+ * print_alphabet_x10 - function to print abc 10 times
  *
- *   * all charcters are in small letters.
- *
- *    * Return: 0
- *
- * */
-void print_alphabet(void)
+ * Return: 0
+ */
+void print_alphabet_x10(void)
 {
-char c;			
-for(c = 'a'; c <= 'z'; ++c)
-{
-putchar(c);
+	char c, i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
 }
-putchar('\n');
-}
-
-
-
-
-
